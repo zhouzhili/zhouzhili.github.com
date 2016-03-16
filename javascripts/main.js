dbb21d4caa91e33bb44ca1dd354acde4eee9cd1b
zhouzhili <1243976730@qq.com>
2016-03-16 14:53:14 +0800

    verson 1.0
    修改默认主页为自己的主页，

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+
+function addAdvice() {
+    var adviceContent = document.getElementById('adviceArea').value;
+    var adviceDiv = document.getElementById('adviceDiv');
+    if (adviceContent) {
+        var p = document.createElement('p');
+        var str = document.createTextNode(adviceContent);
+        p.appendChild(str);
+        adviceDiv.appendChild(p);
+        var hr = document.createElement('hr');
+        adviceDiv.appendChild(hr);
+        document.getElementById('adviceArea').value = null;
+    }
+}
